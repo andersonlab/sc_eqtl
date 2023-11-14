3b4e172e251833880d0a5b260da00f6e618d9ba8
BradleyH017 <bh18@sanger.ac.uk>
2023-11-14 16:39:39 +0000

    Setting up conda

--- setup_env.sh
+++ setup_env.sh
@@ -5,4 +5,9 @@
 # And https://github.com/weizhou0/qtl/blob/7f54b239db26a37851208d2ca4961b63f8fae2d9/extdata/step2_tests_qtl.R#L4
 # As described in https://github.com/annacuomo/SAIGE_QTL_analyses/blob/main/saige_qtl_runners/qtl_cmd.sh
 
+# Clone the relevent environment (SAIGEQTL) [Change depending on where you keep scripts etc]
+cd /lustre/scratch126/humgen/projects/sc-eqtl-ibd/analysis/bradley_analysis/scripts/scRNAseq/SAIGE/qtl/conda_env
+git clone git@github.com:weizhou0/qtl.git
 
+# Install from environment yaml (following qtl/conda_env/createCondaEnvSAIGE_steps.txt)
+mamba env create -f qtl/conda_env/environment-RSAIGE.yml
