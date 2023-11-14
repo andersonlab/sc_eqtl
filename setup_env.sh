9048c3c3c2fc96dc88e89a1407aa368319d0041d
BradleyH017 <bh18@sanger.ac.uk>
2023-11-14 11:11:12 +0000

    Initial commit

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+# Bradley 14/11/23
+# Setting up the conda/mamba environment to run SAIGETL
+# This method is an application of SAIGE (https://github.com/weizhouUMICH/SAIGE), to run eQTL analyses on scRNAseq data
+# It runs using code from: https://github.com/weizhou0/qtl/blob/7f54b239db26a37851208d2ca4961b63f8fae2d9/extdata/step1_fitNULLGLMM_qtl.R#L4
+# And https://github.com/weizhou0/qtl/blob/7f54b239db26a37851208d2ca4961b63f8fae2d9/extdata/step2_tests_qtl.R#L4
+# As described in https://github.com/annacuomo/SAIGE_QTL_analyses/blob/main/saige_qtl_runners/qtl_cmd.sh
+
+
